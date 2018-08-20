838126631b382ff89df94b4f963d929f74e4a736
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-08-20 18:51:14 -0500

    Update Numpy-Arrays.py

--- Chapter_2/Numpy-Arrays.py
+++ Chapter_2/Numpy-Arrays.py
@@ -3,4 +3,8 @@ Additional helpful Numpy examples from:
  https://towardsdatascience.com/a-hitchhiker-guide-to-python-numpy-arrays-9358de570121
 '''
 
+# Numpy is useful for homogenous datasets, using multiple types won't work
+numpy_arr = np.array([1,2,"Hello",3,"World"], dtype=np.int32)  # Error
 
+# A python list can handle a variety of types 
+py_arr = [1,2,"Hello",3,"World"] # Valid

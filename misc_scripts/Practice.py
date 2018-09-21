bec362d52f202adc417f53d3eaf02a9d53705a3d
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-09-21 16:14:23 -0400

    Initial commit
    Add class, additional data structures,
    Define more complex data structures (Linked List in Python)

---
+++ misc_scripts/Practice.py
@@ -0,0 +1,20 @@
+# Define a class
+
+
+x = 2
+
+# Define a function
+def squared_x(x):
+	y = x ** 2
+	return y
+
+print(squared_x(x))
+
+
+# Define a list
+a1 = []
+a2 = list()
+
+# Define a Dict
+b1 = {}
+b2 = dict()

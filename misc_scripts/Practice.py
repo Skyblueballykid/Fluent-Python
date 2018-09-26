48200a4f8981f6a64050ad8fce129a6b6ac0241b
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-09-26 13:24:31 -0500

    Updated with list and dict comp examples

--- misc_scripts/Practice.py
+++ misc_scripts/Practice.py
@@ -1,21 +1,3 @@
-# Define a class
-class StreetDog:
-	def __init__(self, name, age, fur):
-		self.name = name
-		self.age = age
-		self.fur = fur
-
-	def printme(self):
-		print("Name: ", self.name, "Age: ", self.age, "Color: ", self.fur)
-
-dog1 = StreetDog("Juno","5 months", "Brown-orange")
-dog2 = StreetDog("Chiim", "6 months", "Orange-Brown")
-
-print("Street Doges\n")
-dog1.printme()
-dog2.printme()
-print('\n')
-
 x = 2
 
 # Define a function
@@ -33,3 +15,11 @@ a2 = list()
 # Define a Dict
 b1 = {}
 b2 = dict()
+
+# List Comp
+a3 = [x for x in range(10)]
+print(a3)
+
+# Dict comp
+b3 = {str(x):x for x in a3}
+print(b3)

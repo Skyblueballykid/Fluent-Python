835905a3567a8242635a45f8c9bb50b2bfba1c82
Thomas Kalnik <kalnik.thomas@gmail.com>
2018-09-26 13:18:41 -0500

    simple func

--- misc_scripts/Practice.py
+++ misc_scripts/Practice.py
@@ -1,14 +1,29 @@
 # Define a class
+class StreetDog:
+	def __init__(self, name, age, fur):
+		self.name = name
+		self.age = age
+		self.fur = fur
 
+	def printme(self):
+		print("Name: ", self.name, "Age: ", self.age, "Color: ", self.fur)
+
+dog1 = StreetDog("Juno","5 months", "Brown-orange")
+dog2 = StreetDog("Chiim", "6 months", "Orange-Brown")
+
+print("Street Doges\n")
+dog1.printme()
+dog2.printme()
+print('\n')
 
 x = 2
 
 # Define a function
 def squared_x(x):
 	y = x ** 2
-	return y
+	print(y)
 
-print(squared_x(x))
+squared_x(x)
 
 
 # Define a list
